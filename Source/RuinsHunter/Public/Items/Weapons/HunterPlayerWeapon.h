c26d2305fdb5d4a8f7a7ea773cc9fa7a7b55998a
Hyunwc <107088369+Hyunwc@users.noreply.github.com>
2025-04-10 14:10:06 +0900

    WeaponBase
    1. WeaponBase와 PlayerWeapon 클래스 추가
    2. 무기를 스폰하는 게임어빌리티 추가
    3. 플레이어 스켈레탈메시에 해머 소켓 추가

---
+++ Source/RuinsHunter/Public/Items/Weapons/HunterPlayerWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Weapons/HunterWeaponBase.h"
+#include "HunterPlayerWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUINSHUNTER_API AHunterPlayerWeapon : public AHunterWeaponBase
+{
+	GENERATED_BODY()
+	
+};
